76485cc72054583a087a4ec150606a6951ff0828
Betuel <betuelsilva159@gmail.com>
2025-05-27 14:27:09 -0500

    añadi la base del proyecto django  de connect

---
+++ connect/usuarios/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def principal(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
