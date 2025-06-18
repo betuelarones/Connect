317d94c32b2cccfa2609541f879853d9de4f2428
Betuel A. <165334923+betuelarones@users.noreply.github.com>
2025-06-17 23:07:01 -0500

    Update settings.py
    Edite el ALLOWED_HOSTS

--- connect/connect/settings.py
+++ connect/connect/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-1eq3hhhg(3$a0a^7j+%ofa131#e%!mh^lu$gp0!8m%1(r#0mu1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['connect-production-fea1.up.railway.app']
 
 
 # Application definition
