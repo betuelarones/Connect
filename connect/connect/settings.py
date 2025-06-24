6cc12f5c8e5fd6350d7f700bcd8c67d81f560dc1
Betuel <betuelsilva159@gmail.com>
2025-06-24 00:59:50 -0500

    fix: allow Railway domain

--- connect/connect/settings.py
+++ connect/connect/settings.py
@@ -130,7 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
